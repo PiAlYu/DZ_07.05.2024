c13840b6dde79cbd2d45430d4003c76952a70e32
aypir <aypirogov@gmail.com>
2024-05-09 19:03:54 +0300

    № 1

--- 24/24.py
+++ 24/24.py
@@ -1,14 +1,11 @@
 fin = open('24.txt')
 a = fin.readline()
 fin.close()
-indx, indy, m = [0, 0], [0, 0], 0
+indx, indy, m = [-1, -1], [-1, -1], 0
 for i in range(len(a)):
     if a[i] == 'X':
-        m = max(m, i - max(indx[1], indy[0]))
-        indx[0] = indx[1]
-        indx[1] = i
+        indx[0], indx[1] = indx[1], i
     elif a[i] == 'Y':
-        m = max(m, i - max(indx[0], indy[1]))
-        indy[0] = indy[1]
-        indy[1] = i
+        indy[0], indy[1] = indy[1], i
+    m = max(m, i - max(indx[0], indy[0]))
 print(m)
